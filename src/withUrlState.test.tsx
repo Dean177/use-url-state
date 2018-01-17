141db25ff85d9197231cd6e94bfa8d30bb1a9030
Dean Merchant <deanmerchant@gmail.com>
2018-01-17 21:27:52 +0000

    Fixes #46 - Add additional test to cover initial url state from existing query params

--- src/withUrlState.test.tsx
+++ src/withUrlState.test.tsx
@@ -5,55 +5,90 @@ import { createBrowserHistory, History, LocationDescriptorObject } from 'history
 import * as queryString from 'query-string'
 import { withUrlState, UrlStateProps } from './withUrlState'
 
-type ControlState = { color: string }
+type ControlState = { animal?: string, color: string }
 declare const global: { window: any }
+
+const UrlBasedControls = (props: UrlStateProps<ControlState>) => (
+  <div>
+    <div className="currentAnimal">{props.urlState.animal}</div>
+    <div className="currentColor">{props.urlState.color}</div>
+    <button className="Green" onClick={() => props.setUrlState({ color: 'Green' })}>
+      Green
+    </button>
+    <button className="Red" onClick={() => props.setUrlState({ color: 'Red' })}>
+      Red
+    </button>
+  </div>
+)
 
 describe('withUrlState', () => {
   let testHistory: History = null as any
-  let UrlBasedControls: any
-  let UrlConnectedControls: any
 
   beforeEach(() => {
     testHistory = createBrowserHistory()
 
     const newLocation: LocationDescriptorObject = {
       ...testHistory.location,
-      search: queryString.stringify({ color: 'blue' }),
+      search: queryString.stringify({ color: 'Blue' }),
     }
     testHistory.replace(newLocation)
+  })
 
-    UrlBasedControls = (props: UrlStateProps<ControlState>) => (
-      <div>
-        <div className="currentState">{props.urlState.color}</div>
-        <button className="Green" onClick={() => props.setUrlState({ color: 'Green' })}>
-          Green
-        </button>
-        <button className="Red" onClick={() => props.setUrlState({ color: 'Red' })}>
-          Red
-        </button>
-      </div>
-    )
-
-    UrlConnectedControls =
+  it('will not override any params which are already provided in the query string', () => {
+    const UrlConnectedControls: any =
       withUrlState<{}, ControlState>(testHistory, () => ({ color: 'Red' }))(UrlBasedControls)
+    expect(queryString.parse(testHistory.location.search)).toEqual({ color: 'Blue' })
+
+    const wrapper = mount(<UrlConnectedControls />)
+
+    expect(queryString.parse(testHistory.location.search)).toEqual({ color: 'Blue' })
+    expect(wrapper.find('.currentColor').text()).toBe('Blue')
+  })
+
+  it('will append any additional any params which are not provided in the querystring', () => {
+    const UrlConnectedControls: any =
+      withUrlState<{}, ControlState>(testHistory, () => ({ animal: 'Ant', color: 'Blue' }))(UrlBasedControls)
+    expect(queryString.parse(testHistory.location.search)).toEqual({ color: 'Blue' })
+
+    const wrapper = mount(<UrlConnectedControls />)
+
+    expect(queryString.parse(testHistory.location.search)).toEqual({ animal: 'Ant', color: 'Blue' })
+    expect(wrapper.find('.currentAnimal').text()).toBe('Ant')
+    expect(wrapper.find('.currentColor').text()).toBe('Blue')
   })
 
   it('sets the url with the initial state', () => {
+    const UrlConnectedControls: any =
+      withUrlState<{}, ControlState>(testHistory, () => ({ animal: 'Ant', color: 'Blue' }))(UrlBasedControls)
+    expect(queryString.parse(testHistory.location.search)).toEqual({ color: 'Blue' })
+
     mount(<UrlConnectedControls />)
-    expect(queryString.parse(testHistory.location.search)).toEqual({ color: 'Red' })
-    expect(queryString.parse(global.window.location.search)).toEqual({ color: 'Red' })
+
+    expect(queryString.parse(testHistory.location.search)).toEqual({ animal: 'Ant', color: 'Blue' })
   })
 
   it('provides the current urls state to the wrapped component', () => {
+    const UrlConnectedControls: any =
+      withUrlState<{}, ControlState>(testHistory, () => ({ animal: 'Ant', color: 'Blue' }))(UrlBasedControls)
+
     const wrapper = mount(<UrlConnectedControls />)
-    expect(wrapper.find('.currentState').text()).toBe('Red')
+
+    expect(wrapper.find('.currentAnimal').text()).toBe('Ant')
+    expect(wrapper.find('.currentColor').text()).toBe('Blue')
   })
 
   it('updates the url when the wrapped component updates the state', () => {
+    const UrlConnectedControls: any =
+      withUrlState<{}, ControlState>(testHistory, () => ({ animal: 'Ant', color: 'Blue' }))(UrlBasedControls)
+
     const wrapper = mount(<UrlConnectedControls />)
-    expect(queryString.parse(testHistory.location.search)).toEqual({ color: 'Red' })
+    expect(wrapper.find('.currentAnimal').text()).toBe('Ant')
+    expect(wrapper.find('.currentColor').text()).toBe('Blue')
+    expect(queryString.parse(testHistory.location.search)).toEqual({ animal: 'Ant', color: 'Blue' })
 
     wrapper.find('.Green').simulate('click')
-    expect(queryString.parse(testHistory.location.search)).toEqual({ color: 'Green' })
+    expect(wrapper.find('.currentAnimal').text()).toBe('Ant')
+    expect(wrapper.find('.currentColor').text()).toBe('Green')
+    expect(queryString.parse(testHistory.location.search)).toEqual({ animal: 'Ant', color: 'Green' })
   })
 })
