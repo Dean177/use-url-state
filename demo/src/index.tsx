b02568e927f9e76231d13a7045e95fccd880228b
Dean Merchant <deanmerchant@gmail.com>
2018-11-16 19:57:20 +0000

    Fix CI config, update wording in readme.

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,8 +1,1 @@
 import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-
-render(<App />, document.getElementById('root') as HTMLElement)
-
-registerServiceWorker()
